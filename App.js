5506296ff85b951b1f0b0eaed14771173815857f
rajatgudi <rajatgudigar123@gmail.com>
2022-03-30 11:11:51 +0530

    initial commiy

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
+  const [FocusObject, setFocusObject] = useState(null);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {FocusObject ? (
+        <Text>I have Focused Text</Text>
+      ) : (
+        <Text>I do Not have focused text</Text>
+      )}
     </View>
   );
 }
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
